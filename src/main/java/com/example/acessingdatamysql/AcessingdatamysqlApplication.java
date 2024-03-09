d10ce2ab731a8f5f8a8535d4ddd249586fb3eb59
joaovallmeida <joaovitor8832@gmail.com>
2024-03-08 21:09:29 -0300

    first commit

---
+++ src/main/java/com/example/acessingdatamysql/AcessingdatamysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.acessingdatamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessingdatamysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessingdatamysqlApplication.class, args);
+	}
+
+}
---
+++ src/main/java/main/java/com/example/acessingdatamysql/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package main.java.com.example.acessingdatamysql.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.accessingdatamysql.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
