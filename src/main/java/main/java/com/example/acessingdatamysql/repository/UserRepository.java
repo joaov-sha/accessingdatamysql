c810a0f0b0c19c5f2a702c5a53e050316e034779
joaov-sha <joaovitor8832@gmail.com>
2024-07-28 17:42:02 -0300

    Alterados os arquivos resolvendo conflitos anteriormente existentes

--- src/main/java/main/java/com/example/acessingdatamysql/repository/UserRepository.java
+++ src/main/java/main/java/com/example/acessingdatamysql/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package main.java.com.example.acessingdatamysql.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.accessingdatamysql.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    void save(main.java.com.example.acessingdatamysql.model.User n);
 
 }
